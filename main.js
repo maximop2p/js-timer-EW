0ef8975443931147ffceea2591e11857a1366e4f
viieja <msantoro@fi.uba.ar>
2020-12-03 22:46:23 -0300

    readme

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 // set timer to:
-var countdown_date = new Date("Jan 5, 2021 15:37:25").getTime();
+var countdown_date = new Date("Dec 18, 2020 00:00:00").getTime();
 
 var x = setInterval(function() {
     var now = new Date().getTime();
@@ -11,7 +11,7 @@ var x = setInterval(function() {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
-    document.getElementById("demo").innerHTML = days + " days, " + hours + " hours, " + minutes + " minutes.";
+    document.getElementById("demo").innerHTML = days + " dias, " + hours + " horas y " + minutes + " minutos.";
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("demo").innerHtml = "done";
